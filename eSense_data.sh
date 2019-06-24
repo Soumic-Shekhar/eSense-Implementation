3ed34923c9a2f8e0a14bc28f6cbac64c7f47f382
Soumic Shekhar <soumic.aoyon@northsouth.edu>
2019-06-25 04:49:17 +0600

    IMU data read successful looses BT pair after disconnecting

--- eSense_data.sh
+++ eSense_data.sh
@@ -4,5 +4,11 @@
 
 devices=$(bluetoothctl&)
 eSense="$(echo "$devices" | grep 'eSense')"
+echo "$eSense"
 eSenseMAC="$(echo "$eSense" | grep -o '[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}')"
 echo "$eSenseMAC"
+
+gatttool -b "$eSenseMAC" --char-write-req -a '0x000c' -n 5367020164 --listen
+#gatttool -b "$eSenseMAC" --char-read -a 0x000c
+
+#gatttool --help-all
